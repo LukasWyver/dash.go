b436021502f99960ba623fd5a61c214ce1da7044
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-05-06 01:05:28 -0300

    feat--component/input

---
+++ src/components/Form/Input.tsx
@@ -0,0 +1,30 @@
+import {
+  FormControl,
+  FormLabel,
+  Input as ChakraInput,
+  InputProps as ChakraInputProps,
+} from '@chakra-ui/react';
+
+interface InputProps extends ChakraInputProps {
+  name: string;
+  label?: string;
+}
+
+export function Input({ name, label, ...rest }: InputProps) {
+  return (
+    <FormControl>
+      {!!label && <FormLabel htmlFor={name}>{label}</FormLabel>}
+      <ChakraInput
+        size="lg"
+        name={name}
+        bg="gray.900"
+        variant="filled"
+        _hover={{
+          bg: 'gray.900',
+        }}
+        focusBorderColor="green.500"
+        {...rest}
+      />
+    </FormControl>
+  );
+}
