73184784bf65a6f40b57744ae459c03016c420e9
Lucas Gimenes <lucas_sin5@hotmail.com>
2023-05-08 23:50:14 -0300

    feat--update-context-useSidebarDrawer

--- src/contexts/SidebarDrawerContext.tsx
+++ src/contexts/SidebarDrawerContext.tsx
@@ -1,5 +1,6 @@
+import { useRouter } from 'next/router';
 import { useDisclosure, UseDisclosureReturn } from '@chakra-ui/react';
-import { ReactNode, createContext, useContext } from 'react';
+import { ReactNode, createContext, useContext, useEffect } from 'react';
 
 interface SidebarDrawerProviderProps {
   children: ReactNode;
@@ -13,6 +14,12 @@ export function SidebarDrawerProvider({
   children,
 }: SidebarDrawerProviderProps) {
   const disclosure = useDisclosure();
+  const router = useRouter();
+
+  useEffect(() => {
+    disclosure.onClose();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.asPath]);
 
   return (
     <SidebarDrawerContext.Provider value={disclosure}>
